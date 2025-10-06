b7bf2470d9dbaa4d996537affc9abc2f344225fc
Rita <ritamendesazevedo@gmail.com>
2025-10-06 19:39:41 +0100

    shuffle, play next song fix

--- MusicApp/app/src/main/java/com/example/musicapp/Player.kt
+++ MusicApp/app/src/main/java/com/example/musicapp/Player.kt
@@ -7,35 +7,32 @@ import android.os.Handler
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlinx.coroutines.newSingleThreadContext
 import java.io.File
 
-//REFERENCE FOR THIS PROJECT:   Coding with Evan on Youtube
 class Player : AppCompatActivity() {
 
     private lateinit var btnPlay: Button
     private lateinit var btnNext: Button
     private lateinit var btnPrev: Button
-    private lateinit var btnFf: Button
-    private lateinit var btnFr: Button
+    private lateinit var btnShuffle: Button
+    private var Shuffle = false
 
     private lateinit var txtSName: TextView
     private lateinit var txtStart: TextView
     private lateinit var txtStop: TextView
 
     private lateinit var seekMusic: SeekBar
-    //private lateinit var visualizer: BarVisualizer
 
     private var sname: String? = null
     private var position: Int = 0
     private var mySongs: ArrayList<File>? = null
 
     private var updateseekbar: Thread? = null
-
 
     companion object {
         var mediaPlayer: MediaPlayer? = null
@@ -55,16 +52,13 @@ class Player : AppCompatActivity() {
         btnPrev = findViewById(R.id.previousButton)
         btnNext = findViewById(R.id.nextButton)
         btnPlay = findViewById(R.id.playButton)
-        btnFf = findViewById(R.id.fastforwardButton)
-        btnFr = findViewById(R.id.previousforwardButton)
-
+        btnShuffle = findViewById(R.id.shuffleButton)
         txtSName = findViewById(R.id.txtsn)
         txtStart = findViewById(R.id.txtsstart)
         txtStop = findViewById(R.id.txtsend)
-
         seekMusic = findViewById(R.id.seekbar)
 
-
+        //stop previous player
         mediaPlayer?.let {
             it.stop()
             it.release()
@@ -76,140 +70,108 @@ class Player : AppCompatActivity() {
         position = intent.getIntExtra("pos", 0)
 
         mySongs = songList?.map { File(it) } as ArrayList<File>?
-
         sname = songName
+
         txtSName.text = sname
         txtSName.isSelected = true
+        playSong(position)
 
-
-        val uri = Uri.parse(mySongs?.get(position)?.toString())
-        mediaPlayer = MediaPlayer.create(applicationContext, uri)
-        mediaPlayer?.start()
-        btnPlay.setBackgroundResource(R.drawable.pause)
-
-        //udapte the song and bar
-        mediaPlayer?.setOnCompletionListener {
-            btnNext.performClick()
-        }
-        updateseekbar = Thread {
-            try {
-                val totalDuration = mediaPlayer?.duration ?: 0
-                var currentPosition = 0
-
-                while (mediaPlayer != null && currentPosition < totalDuration) {
-                    try {
-                        Thread.sleep(500) //0,5s
-                        currentPosition = mediaPlayer!!.currentPosition
-                        runOnUiThread {
-                            seekMusic.progress = currentPosition
-                        }
-                    } catch (e: InterruptedException) {
-                        e.printStackTrace()
-                        break
-                    } catch (e: IllegalStateException) {
-                        e.printStackTrace()
-                        break
-                    }
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-        seekMusic.max = mediaPlayer?.duration ?: 0
-        updateseekbar?.start()
-        seekMusic.progressDrawable.setColorFilter(
-            resources.getColor(R.color.primarycolor, null),
-            android.graphics.PorterDuff.Mode.MULTIPLY
-        )
-        seekMusic.thumb.setColorFilter(
-            resources.getColor(R.color.primarycolor, null),
-            android.graphics.PorterDuff.Mode.SRC_IN
-        )
-        seekMusic.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                //the user "updates" the song moving the bar
-                if (fromUser) {
-                    mediaPlayer?.seekTo(progress)
-                }
-            }
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
-            }
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                mediaPlayer?.seekTo(seekBar?.progress ?: 0)
-
-            }
-        })
-
-        val endTime = createTime(mediaPlayer?.duration ?: 0)
-        txtStop.text = endTime
-
+        //seekbar
         val handler = Handler()
         val delay: Long = 1000
-
         handler.postDelayed(object : Runnable {
             override fun run() {
                 val currentTime = createTime(mediaPlayer?.currentPosition ?: 0)
                 txtStart.text = currentTime
+                seekMusic.progress = mediaPlayer?.currentPosition ?: 0
                 handler.postDelayed(this, delay)
             }
         }, delay)
+        seekMusic.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) mediaPlayer?.seekTo(progress)
+            }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                mediaPlayer?.seekTo(seekBar?.progress ?: 0)
+            }
+        })
 
         btnPlay.setOnClickListener {
-            if (mediaPlayer?.isPlaying == true) {
-                btnPlay.setBackgroundResource(R.drawable.play)
-                mediaPlayer?.pause()
-            } else {
-                btnPlay.setBackgroundResource(R.drawable.pause)
-                mediaPlayer?.start()
+            mediaPlayer?.let {
+                if (it.isPlaying) {
+                    it.pause()
+                    btnPlay.setBackgroundResource(R.drawable.play)
+                } else {
+                    it.start()
+                    btnPlay.setBackgroundResource(R.drawable.pause)
+                }
             }
         }
 
-        btnNext.setOnClickListener {
-            mediaPlayer?.stop()
-            mediaPlayer?.release()
-            position = (position + 1) % mySongs!!.size
-            val uri2= Uri.parse(mySongs?.get(position)?.toString())
-            mediaPlayer = MediaPlayer.create(applicationContext, uri2)
-            //remove the extensions name
-            sname = mySongs!![position].name.substringBeforeLast(".")
-            txtSName.text = sname
 
-            seekMusic.progress = 0
-            seekMusic.max = mediaPlayer?.duration ?: 0
-            mediaPlayer?.start()
-            btnPlay.setBackgroundResource(R.drawable.pause)
+        btnNext.setOnClickListener {
+            position = if (Shuffle) {
+                (0 until mySongs!!.size).random()
+            } else {
+                (position + 1) % mySongs!!.size
+            }
+            playSong(position)
         }
 
         btnPrev.setOnClickListener {
-            mediaPlayer?.stop()
-            mediaPlayer?.release()
-            //to prevent -1
-            position = if (position - 1 < 0) {
-                mySongs!!.size - 1
+            position = if (Shuffle) {
+                (0 until mySongs!!.size).random()
             } else {
-                position - 1
+                if (position - 1 < 0) mySongs!!.size - 1 else position - 1
             }
-            val uri2= Uri.parse(mySongs?.get(position)?.toString())
-            mediaPlayer = MediaPlayer.create(applicationContext, uri2)
-            //remove the extensions name
-            sname = mySongs!![position].name.substringBeforeLast(".")
-            txtSName.text = sname
-
-            seekMusic.progress = 0
-            seekMusic.max = mediaPlayer?.duration ?: 0
-
-            mediaPlayer?.start()
-            btnPlay.setBackgroundResource(R.drawable.pause)
+            playSong(position)
         }
 
+        btnShuffle.setOnClickListener {
+            Shuffle = !Shuffle
+            if (Shuffle) {
+                btnShuffle.setBackgroundResource(R.drawable.shuffleon)
+                Toast.makeText(this,"Shuffle turned on", Toast.LENGTH_SHORT).show()
+            } else {
+                btnShuffle.setBackgroundResource(R.drawable.shuffleoff)
+                Toast.makeText(this,"Shuffle turned off", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
-    fun createTime(duration: Int): String {
+
+    private fun playSong(pos: Int) {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer=null
+
+        val uri = Uri.fromFile(mySongs!![pos])
+        mediaPlayer = MediaPlayer.create(applicationContext, uri)
+        mediaPlayer?.start()
+
+        sname = mySongs!![pos].name.substringBeforeLast(".")
+        txtSName.text = sname
+        btnPlay.setBackgroundResource(R.drawable.pause)
+
+        val duration = mediaPlayer?.duration ?: 0
+        txtStop.text = createTime(duration)
+        seekMusic.progress = 0
+        seekMusic.max = duration
+
+        mediaPlayer?.setOnCompletionListener {
+            position = if (Shuffle) {
+                (0 until mySongs!!.size).random()
+            } else {
+                (position + 1) % mySongs!!.size
+            }
+            playSong(position)
+        }
+    }
+
+    private fun createTime(duration: Int): String {
         val min = duration / 1000 / 60
         val sec = duration / 1000 % 60
         return String.format("%d:%02d", min, sec)
     }
-
-
 }
